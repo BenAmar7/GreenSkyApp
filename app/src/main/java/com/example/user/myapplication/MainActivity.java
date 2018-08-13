039ba4e2669e12ec1dc1d3a7d0678f2f3af8823b
Ran <ran.ben.amar77@gmail.com>
2018-08-13 16:52:43 +0300

    Fire base almost work

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -3,11 +3,49 @@ package com.example.user.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference databaseReference = database.getReference();
+
+
+            ValueEventListener postListener = new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // Get Post object and use the values to update the UI
+                    User user = dataSnapshot.getValue(User.class);
+                    // ...
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    // Getting Post failed, log a message
+                    //Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                    // ...
+                }
+            };
+            databaseReference.addValueEventListener(postListener);
+
+
+            User user1 = new User("koko");
+            User user2 = new User("lolo");
+            databaseReference.child("Users").child("1").setValue(user1);
+            databaseReference.child("Users").child("2").setValue(user2);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/com/example/user/myapplication/User.java
@@ -0,0 +1,12 @@
+package com.example.user.myapplication;
+
+public class User {
+    String name;
+    int points;
+
+    public User(String name) {// int[] flightIDS){
+        this.name = name;
+        this.points = 0;
+    }
+}
+
